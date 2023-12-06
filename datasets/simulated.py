df7a7ea12614374271e8b0ace0aebf9426a9b5cf
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-12-06 16:40:30 +0100

    objective & datasets

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,43 +1,29 @@
-from benchopt import BaseDataset, safe_import_context
+from benchopt import BaseDataset
+from benchopt import safe_import_context
 
+from benchopt.datasets.simulated import make_correlated_data
 
-# Protect the import with `safe_import_context()`. This allows:
-# - skipping import to speed up autocompletion in CLI.
-# - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
 
 
-# All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
-
-    # Name to select the dataset in the CLI and to display the results.
     name = "Simulated"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
-        'n_samples, n_features': [
-            (1000, 500),
-            (5000, 200),
-        ],
-        'random_state': [27],
+        'n_samples, n_features': [(100, 3000)],
+        'groups': [10]
     }
 
-    # List of packages needed to run the dataset. See the corresponding
-    # section in objective.py
-    requirements = []
+    def __init__(self, n_samples, n_features, groups, random_state=0):
+        self.n_samples, self.n_features = n_samples, n_features
+        self.groups = groups
+        self.random_state = random_state
 
     def get_data(self):
-        # The return arguments of this function are passed as keyword arguments
-        # to `Objective.set_data`. This defines the benchmark's
-        # API to pass data. It is customizable for each benchmark.
-
-        # Generate pseudorandom data using `numpy`.
         rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
+        X, y, _ = make_correlated_data(
+            self.n_samples, self.n_features, random_state=rng
+        )
 
-        # The dictionary defines the keyword arguments for `Objective.set_data`
-        return dict(X=X, y=y)
+        return dict(X=X, y=y, groups=self.groups)
--- objective.py
+++ objective.py
@@ -1,79 +1,69 @@
-from benchopt import BaseObjective, safe_import_context
+from benchopt import BaseObjective
+from benchopt import safe_import_context
 
-# Protect the import with `safe_import_context()`. This allows:
-# - skipping import to speed up autocompletion in CLI.
-# - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
+    from numpy.linalg import norm
+
+    from skglm.datafits import QuadraticGroup
+    from skglm.penalties import WeightedGroupL2
+    from skglm.utils.data import grp_converter
 
 
-# The benchmark objective must be named `Objective` and
-# inherit from `BaseObjective` for `benchopt` to work properly.
 class Objective(BaseObjective):
+    name = "Group Lasso objective"
 
-    # Name to select the objective in the CLI and to display the results.
-    name = "Ordinary Least Squares"
+    requiremens = ["pip:skglm"]
 
-    # URL of the main repo for this benchmark.
-    url = "https://github.com/#ORG/#BENCHMARK_NAME"
-
-    # List of parameters for the objective. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    # All parameters 'p' defined here are available as 'self.p'.
-    # This means the OLS objective will have a parameter `self.whiten_y`.
     parameters = {
-        'whiten_y': [False, True],
+        'reg': [1., 1e-1, 1e-2, 1e-3],
     }
 
-    # List of packages needed to run the benchmark.
-    # They are installed with conda; to use pip, use 'pip:packagename'. To
-    # install from a specific conda channel, use 'channelname:packagename'.
-    # Packages that are not necessary to the whole benchmark but only to some
-    # solvers or datasets should be declared in Dataset or Solver (see
-    # simulated.py and python-gd.py).
-    # Example syntax: requirements = ['numpy', 'pip:jax', 'pytorch:pytorch']
-    requirements = []
+    def __init__(self, reg):
+        self.reg = reg
 
-    # Minimal version of benchopt required to run this benchmark.
-    # Bump it up if the benchmark depends on a new feature of benchopt.
-    min_benchopt_version = "1.5"
+    def set_data(self, X, y, groups):
+        _, n_features = X.shape
 
-    def set_data(self, X, y):
-        # The keyword arguments of this function are the keys of the dictionary
-        # returned by `Dataset.get_data`. This defines the benchmark's
-        # API to pass data. This is customizable for each benchmark.
+        grp_indices, grp_ptr = grp_converter(groups, n_features)
+        n_groups = len(grp_ptr) - 1
+        weights = np.ones(n_groups)
+
+        lmbd_max = self._compute_lmbd_max(X, y, grp_indices, grp_ptr)
+        self.lmbd = self.reg * lmbd_max
+
         self.X, self.y = X, y
+        self.grp_indices, self.grp_ptr = grp_indices, grp_ptr
 
-        # `set_data` can be used to preprocess the data. For instance,
-        # if `whiten_y` is True, remove the mean of `y`.
-        if self.whiten_y:
-            y -= y.mean(axis=0)
-
-    def evaluate_result(self, beta):
-        # The keyword arguments of this function are the keys of the
-        # dictionary returned by `Solver.get_result`. This defines the
-        # benchmark's API to pass solvers' result. This is customizable for
-        # each benchmark.
-        diff = self.y - self.X @ beta
-
-        # This method can return many metrics in a dictionary. One of these
-        # metrics needs to be `value` for convergence detection purposes.
-        return dict(
-            value=.5 * diff @ diff,
+        self.datafit = QuadraticGroup(grp_ptr, grp_indices)
+        self.penalty = WeightedGroupL2(
+            self.lmbd, weights, grp_ptr, grp_indices
         )
 
-    def get_one_result(self):
-        # Return one solution. The return value should be an object compatible
-        # with `self.evaluate_result`. This is mainly for testing purposes.
-        return dict(beta=np.zeros(self.X.shape[1]))
+    def evaluate_result(self, w):
+        X, y = self.X, self.y
+        datafit, penalty = self.datafit, self.penalty
+
+        return dict(
+            value=datafit.value(y, w, X @ w) + penalty.value(w)
+        )
 
     def get_objective(self):
-        # Define the information to pass to each solver to run the benchmark.
-        # The output of this function are the keyword arguments
-        # for `Solver.set_objective`. This defines the
-        # benchmark's API for passing the objective to the solver.
-        # It is customizable for each benchmark.
         return dict(
-            X=self.X,
-            y=self.y,
+            X=self.X, y=self.y, alpha=self.alpha, groups=self.groups,
+            grp_indices=self.grp_indices, grp_ptr=self.grp_ptr
         )
+
+    def _compute_lmbd_max(self, X, y, grp_indices, grp_ptr):
+        lmbd_max = 0.
+        n_samples = X.shape[0]
+        n_groups = len(grp_ptr) - 1
+
+        for g in range(n_groups):
+            grp_g_indices = grp_indices[grp_ptr[g]: grp_ptr[g+1]]
+            lmbd_max = max(
+                lmbd_max,
+                norm(X[:, grp_g_indices].T @ y) / n_samples
+            )
+
+        return lmbd_max
